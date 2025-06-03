2d7eed36b6c7d5a7121c49bf2b988fc705651aae
Fox5091 <70080711+Fox5091@users.noreply.github.com>
2025-06-03 11:26:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -158,6 +158,8 @@ def get_acunt_books_by_period(acunt_book_name):
     conn.close()
     return jsonify(acunt_books)
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
